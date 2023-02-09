2f3ceac31343dc98f318ef0b09feecc5e686bde5
Brian C <brian_canon@cun.edu.co>
2023-02-09 16:52:02 -0500

    Agregar archivos de proyecto.

---
+++ Models/SaveCommentModel.cs
@@ -0,0 +1,11 @@
+namespace AmarisPrueba.Models
+{
+    public class SaveCommentModel
+    {
+        public Guid Id { get; set; }
+        public string CommentAuthor { get; set; }
+        public string CommentEmail { get; set; }
+        public string CommentUrl { get; set; }
+        public string CommentBody { get; set; }
+    }
+}
---
+++ Models/StandardPost.cs
@@ -0,0 +1,15 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace AmarisPrueba.Models
+{
+    [PostType(Title = "Standard post")]
+    public class StandardPost : Post<StandardPost>
+    {
+        /// <summary>
+        /// Gets/sets the available comments if these
+        /// have been loaded from the database.
+        /// </summary>
+        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
